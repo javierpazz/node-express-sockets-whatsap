0cd4f31bb2f293e7c7480272e9ebdeefeb512012
JAVIER <javierpia0105@gmail.com>
2023-04-24 09:00:43 -0300

    Start App

---
+++ constants.js
@@ -0,0 +1,11 @@
+export const DB_USER = 'admin';
+export const DB_PASSWORD = 'admin123456';
+export const DB_HOST = 'chatapp.fe0krcf.mongodb.net';
+
+export const IP_SERVER = 'localhost';
+
+export const JWT_SECRET_KEY = 'gR7cH9Svfj8JLe4c186Ghs48hheb3902nh5DsA';
+
+export const PORT = process.env.PORT || 3977;
+
+//mongodb+srv://admin:<password>@chatapp.fe0krcf.mongodb.net/?retryWrites=true&w=majority
